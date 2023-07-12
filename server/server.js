2e4b6ebe4f5724ea441979dc0a2a098889d580ed
Jared Gergen <gergen.jared1@gmail.com>
2023-07-11 17:02:09 -0700

    Implemented appolo server and applied it as middleware to the express server.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./middleware/auth");
+const db = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 3001;
+
+  await db.connect();
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`GraphQL endpoint: ${server.graphqlPath}`);
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();
